aa2d86365baa80941092709aadeaf715f1e6b4ea
Jerome <me@hp>
2024-01-26 10:15:41 +0100

    oops2

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -96,6 +96,10 @@ export const modelCost = {
     prompt: { price: 0.01, unit: 1000 },
     completion: { price: 0.03, unit: 1000 },
   },
+  'gpt-4-2501-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 },
+  },
 };
 
 export const defaultUserMaxToken = 4000;
