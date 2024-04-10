84135868c21dc7f19038214925e29981093b4f6d
freejazz13 <jerome.outhier@gmail.com>
2024-04-10 12:36:19 +0200

    Update chat.ts turbo4

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -22,7 +22,8 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo-16k',
   'gpt-4',
   'gpt-4-32k',
-  'gpt-4-1106-preview',
+  'gpt-4-1106-preview,
+  'gpt-4-turbo,
   'gpt-4-0125-preview'
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
@@ -45,6 +46,7 @@ export const modelMaxToken = {
   'gpt-4-32k-0613': 32768,
   'gpt-4-1106-preview': 128000,
   'gpt-4-0125-preview': 128000,
+  'gpt-4-turbo': 128000,
 };
 
 export const modelCost = {
@@ -97,6 +99,10 @@ export const modelCost = {
     completion: { price: 0.03, unit: 1000 },
   },
   'gpt-4-0125-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 },
+  },
+  'gpt-4-turbo': {
     prompt: { price: 0.01, unit: 1000 },
     completion: { price: 0.03, unit: 1000 },
   },
